b617d9b362736e35cc1e5f46ea99436dbadff14d
CedricSchacher <cedi.sch@hotmail.ch>
2017-04-19 15:48:44 +0200

    Bilder hochladen
    Bilder hochladen noch nicht funktionell hinzugefügt

---
+++ shareyourlife/controller/BilderController.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once '../repository/BilderRepository.php';
+
+/**
+ * Siehe Dokumentation im DefaultController.
+ */
+class BilderController
+{
+    public function index()
+    {
+        $bilderRepository = new BilderRepository();
+
+        $view = new View('picture_index');
+        $view->title = 'Bilder';
+        $view->heading = 'Bilder';
+        $view->users = $bilderRepository->readAll();
+        $view->display();
+    }
+
+    public function upload()
+    {
+        if (!is_null($_FILES['userfile']['tmp_name']))
+    {
+        echo "<input type='image' src='../public/images/upload/$_FILES['userfile']['tmp_name']''>";
+    }
+        //$view = new View('user_create');
+        //$view->title = 'Benutzer erstellen';
+        //$view->heading = 'Benutzer erstellen';
+        //$view->display();
+    }
+
+    public function doCreate()
+    {
+        if ($_POST['send']) {
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $email = $_POST['email'];
+            // $password  = $_POST['password'];
+            $password = 'no_password';
+
+            $userRepository = new UserRepository();
+            $userRepository->create($firstName, $lastName, $email, $password);
+        }
+
+        // Anfrage an die URI /user weiterleiten (HTTP 302)
+        header('Location: /user');
+    }
+
+    public function delete()
+    {
+        $userRepository = new UserRepository();
+        $userRepository->deleteById($_GET['id']);
+
+        // Anfrage an die URI /user weiterleiten (HTTP 302)
+        header('Location: /user');
+    }
+}
---
+++ shareyourlife/view/picture_index.php
@@ -0,0 +1,9 @@
+<form method="POST" action="/bilder/upload" enctype="multipart/form-data">
+ <input type="file" name="userfile">
+ <input type="submit" value="Upload">
+</form>
+<?php 
+	
+	
+
+?>
